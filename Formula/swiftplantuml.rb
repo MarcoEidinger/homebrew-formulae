a5003d3cc3520082ae48fa5ed2d5a4f2a8dd47ee
Marco Eidinger <marco.eidinger@sap.com>
2021-02-28 19:21:57 -0800

    update swiftplantuml to 0.3.0

--- Formula/swiftplantuml.rb
+++ Formula/swiftplantuml.rb
@@ -1,8 +1,8 @@
 class Swiftplantuml < Formula
   desc "Generate UML class diagrams from Swift sources"
   homepage "https://github.com/MarcoEidinger/SwiftPlantUML"
-  url "https://github.com/MarcoEidinger/SwiftPlantUML/archive/0.2.1.tar.gz"
-  sha256 "49fd20113888acd146865057f36c8ce9c5425971c0747ce6debf1afbac62119b"
+  url "https://github.com/MarcoEidinger/SwiftPlantUML/archive/0.3.0.tar.gz"
+  sha256 "18cdad1efa701c7894620594da78714e205d85d2c81d356fdf276925e88714d0"
   license "MIT"
 
   depends_on xcode: ["12.2", :build]
